e52f068bdf86e12b11ce59479463cddc27046d24
nikitovskij <nbaranovsky24@gmail.com>
2020-08-12 19:19:27 +0200

    init dev_gendiff

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Funct\Collection;
+
 const FIRST_FILE = '<firstFile>';
 const SECOND_FILE = '<secondFile>';
 
@@ -12,10 +14,19 @@ function run($args)
 
     $res = checkDiff($firstFile, $secondFile);
 
-    return genReadableOutput($res);
+    return getReadableOutput($res);
 }
 
-function getFileContent($file)
+function checkDiff($firstFile, $secondFile)
+{
+    $contentFirst = getFileContents($firstFile); //array
+    $contentSecond = getFileContents($secondFile); //array
+    $comparedData = getComparingData($contentFirst, $contentSecond);
+    
+    return $comparedData;
+}
+
+function getFileContents($file)
 {
     $filePath = realpath($file);
     if (file_exists($filePath)) {
@@ -26,48 +37,90 @@ function getFileContent($file)
     return false;
 }
 
-function checkDiff($firstFile, $secondFile)
+function getComparingData(array $contentFirst, array $contentSecond)
 {
-    $contentFirst = getFileContent($firstFile); //array
-    $contentSecond = getFileContent($secondFile); //array
+    $listOfKeys = Collection\union(array_keys($contentFirst), array_keys($contentSecond));
 
     $result = [];
+    $comparingFunc = function ($item) use ($contentFirst, $contentSecond, $result) {
+        $isPairSame = isItemsSame($item, $contentFirst, $contentSecond);
+        $isChanged  = isItemsChanged($item, $contentFirst, $contentSecond);
+        $isAdded    = isItemAdded($item, $contentFirst, $contentSecond);
+        $isDeleted  = isItemDeleted($item, $contentFirst, $contentSecond);
 
-    //TODO: use filter, map, reduce instead
-    foreach ($contentFirst as $key => $value) {
-        foreach ($contentSecond as $skey => $svalue) {
-            if (array_key_exists($key, $contentSecond)) {
-                if ($key === $skey && $value === $svalue) {
-                    $result[$key] = $value;
-                } elseif ($key === $skey && $value !== $svalue) {
-                    $removedItemKey = '- ' . $key;
-                    $addedItemKey = '+ ' . $key;
-                    $result[$removedItemKey] = $value;
-                    $result[$addedItemKey] = $svalue;
-                }
-            } else {
-                $removedItemKey = '- ' . $key;
-                $result[$removedItemKey] = $value;
-            }
+        if ($isPairSame) {
+            $result[$item] = $contentFirst[$item];
+        }
 
-            if (!array_key_exists($skey, $contentFirst)) {
-                $addedItemKey = '+ ' . $skey;
-                $result[$addedItemKey] = $svalue;
-            }
+        if ($isChanged) {
+            $result["- {$item}"] = $contentFirst[$item];
+            $result["+ {$item}"] = $contentSecond[$item];
+        }
+
+        if ($isAdded) {
+            $result["+ {$item}"] = $contentSecond[$item];
+        }
+
+        if ($isDeleted) {
+            $result["- {$item}"] = $contentFirst[$item];
+        }
+
+        return $result;
+    };
+
+    return array_map($comparingFunc, $listOfKeys);
+}
+
+function isItemsSame($key, array $dataFirst, array $dataSecond)
+{
+    if (array_key_exists($key, $dataFirst)) {
+        if (array_key_exists($key, $dataSecond)) {
+            return $dataFirst[$key] === $dataSecond[$key];
         }
     }
 
-    return $result;
+    return false;
 }
 
-function genReadableOutput($data)
+function isItemsChanged($key, array $dataFirst, array $dataSecond)
 {
-    $func = function ($key, $value) {
-        $value = $value === true ? "true" : $value;
-        return "  {$key}: {$value}";
-    };
+    if (array_key_exists($key, $dataFirst)) {
+        if (array_key_exists($key, $dataSecond)) {
+            return $dataFirst[$key] !== $dataSecond[$key];
+        }
+    }
 
-    $readableOutput = array_map($func, array_keys($data), $data);
+    return false;
+}
+
+function isItemAdded($key, array $dataFirst, array $dataSecond)
+{
+    return !array_key_exists($key, $dataFirst) && array_key_exists($key, $dataSecond);
+}
+
+function isItemDeleted($key, array $dataFirst, array $dataSecond)
+{
+    return array_key_exists($key, $dataFirst) && !array_key_exists($key, $dataSecond);
+}
+
+function getReadableOutput($data)
+{
+    $logicData = [
+        true  => "true",
+        false => "false",
+        null  => "null",
+        ''    => "''"
+    ];
+
+    $func = function ($value) use ($logicData) {
+        $flatten = array_map(function ($key, $value) use ($logicData) {
+            $value = $logicData[$value] ?? $value;
+            return "    {$key} : {$value}";
+        }, array_keys($value), $value);
+
+        return $flatten;
+    };
+    $readableOutput = Collection\flatten(array_map($func, $data));
 
     return "{\n" . implode("\n", $readableOutput) . "\n}";
 }
