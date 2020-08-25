294d3c03134acbfb8a07f5860dc43ab503ee565f
nikitovskij <nbaranovsky24@gmail.com>
2020-08-25 17:09:13 +0200

    dev gentree

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -2,134 +2,224 @@
 
 namespace App;
 
+use App\DiffGenerator;
 use Funct\Collection;
-use App\Parsers\JsonParser;
-use App\Parsers\YmlParser;
 
-const FIRST_FILE = '<firstFile>';
-const SECOND_FILE = '<secondFile>';
-
-function run($args)
+function genDiff($filePathOne, $filePathTwo)
 {
-    $firstFile = $args[FIRST_FILE];
-    $secondFile = $args[SECOND_FILE];
-
-    $res = checkDiff($firstFile, $secondFile);
-
-    return getReadableOutput($res);
-}
-
-function checkDiff($firstFile, $secondFile)
-{
-    $contentFirst = getFileContents($firstFile); //array
-    $contentSecond = getFileContents($secondFile); //array
-    $comparedData = getComparingData($contentFirst, $contentSecond);
+    $dataBefore = getFileContent($filePathOne);
+    $treeBefore = makeTree($dataBefore);
     
-    return $comparedData;
+    $dataAfter = getFileContent($filePathTwo);
+    $treeAfter = makeTree($dataAfter);
+    $res = comparingData($treeBefore, $treeAfter);
+    $res = genOutput($res);
+    print_r($res);
+    return $res;
 }
 
-function getFileContents($file)
+
+function comparingData($firstTree, $secondTree)
 {
-    $parsers = [
-        'json' => fn($data) => JsonParser\parseJson($data),
-        'yml'  => fn($data) => YmlParser\parseYml($data)
-    ];
+    $listOfKeysFirstTree  = array_map(fn ($item) => $item['key'], $firstTree);
+    $listOfKeysSecondTree = array_map(fn ($item) => $item['key'], $secondTree);
 
-    $filePath = realpath($file);
-    if (!file_exists($filePath)) {
-        return false;
-    }
+    $listOfKeys = Collection\union($listOfKeysFirstTree, $listOfKeysSecondTree);
 
-    $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
-    $data = file_get_contents($filePath);
+    $checkKeyValue = function ($key) use ($firstTree, $secondTree) {
+        $firstChild  = getCurrentNode($key, $firstTree)['value'] ?? null;
+        $secondChild = getCurrentNode($key, $secondTree)['value'] ?? null;
 
-    return $parsers[$fileExtension]($data);
-}
-
-function getComparingData(array $contentFirst, array $contentSecond)
-{
-    $listOfKeys = Collection\union(array_keys($contentFirst), array_keys($contentSecond));
-
-    $result = [];
-    $comparingFunc = function ($item) use ($contentFirst, $contentSecond, $result) {
-        $isPairSame = isItemsSame($item, $contentFirst, $contentSecond);
-        $isChanged  = isItemsChanged($item, $contentFirst, $contentSecond);
-        $isAdded    = isItemAdded($item, $contentFirst, $contentSecond);
-        $isDeleted  = isItemDeleted($item, $contentFirst, $contentSecond);
-
-        if ($isPairSame) {
-            $result[$item] = $contentFirst[$item];
+        if (is_array($firstChild) && is_array($secondChild)) {
+            return ['key' => $key, 'state' => 'same', 'value' => comparingData($firstChild, $secondChild)];
         }
 
-        if ($isChanged) {
-            $result["- {$item}"] = $contentFirst[$item];
-            $result["+ {$item}"] = $contentSecond[$item];
-        }
-
-        if ($isAdded) {
-            $result["+ {$item}"] = $contentSecond[$item];
-        }
-
-        if ($isDeleted) {
-            $result["- {$item}"] = $contentFirst[$item];
-        }
-
-        return $result;
+        return makeCompareData($key, $firstTree, $secondTree);
     };
 
-    return array_values(array_map($comparingFunc, $listOfKeys));
+    return array_values(array_map($checkKeyValue, $listOfKeys));
 }
 
-function isItemsSame($key, array $dataFirst, array $dataSecond)
+
+
+function makeCompareData($key, $contentFirst, $contentSecond)
 {
-    if (array_key_exists($key, $dataFirst)) {
-        if (array_key_exists($key, $dataSecond)) {
-            return $dataFirst[$key] === $dataSecond[$key];
-        }
+    $nodeFirst  = getCurrentNode($key, $contentFirst);
+    $nodeSecond = getCurrentNode($key, $contentSecond);
+
+    $isPairSame = isItemSame($key, $contentFirst, $contentSecond);
+    $isChanged  = isItemChanged($key, $contentFirst, $contentSecond);
+    $isAdded    = isItemAdded($key, $contentFirst, $contentSecond);
+    $isDeleted  = isItemDeleted($key, $contentFirst, $contentSecond);
+
+    if ($isPairSame) {
+        $node = [$key, 'same', $nodeFirst['value']];
     }
 
-    return false;
+    if ($isChanged) {
+        $node = [$key, 'change', ['before' => $nodeFirst['value'], 'after' => $nodeSecond['value']]];
+    }
+
+    if ($isAdded) {
+        $node = [$key, 'new', $nodeSecond['value']];
+    }
+
+    if ($isDeleted) {
+        $node = [$key, 'delete', $nodeFirst['value']];
+    }
+
+    [$key, $state, $value] = $node;
+
+    return [
+        'key'   => $key,
+        'state' => $state,
+        'value' => $value
+    ];
 }
 
-function isItemsChanged($key, array $dataFirst, array $dataSecond)
+function isItemSame($key, array $dataFirst, array $dataSecond)
 {
-    if (array_key_exists($key, $dataFirst)) {
-        if (array_key_exists($key, $dataSecond)) {
-            return $dataFirst[$key] !== $dataSecond[$key];
-        }
+    $nodeFirst  = getCurrentNode($key, $dataFirst);
+    $nodeSecond = getCurrentNode($key, $dataSecond);
+
+    if (!empty($nodeFirst) && !empty($nodeSecond)) {
+        return $nodeFirst['value'] === $nodeSecond['value'];
     }
 
-    return false;
+    return  false;
+}
+
+function isItemChanged($key, array $dataFirst, array $dataSecond)
+{
+    $nodeFirst  = getCurrentNode($key, $dataFirst);
+    $nodeSecond = getCurrentNode($key, $dataSecond);
+
+    if (!empty($nodeFirst) && !empty($nodeSecond)) {
+        return $nodeFirst['value'] !== $nodeSecond['value'];
+    }
+
+    return  false;
 }
 
 function isItemAdded($key, array $dataFirst, array $dataSecond)
 {
-    return !array_key_exists($key, $dataFirst) && array_key_exists($key, $dataSecond);
+    $nodeFirst  = getCurrentNode($key, $dataFirst);
+    $nodeSecond = getCurrentNode($key, $dataSecond);
+
+    return empty($nodeFirst) && !empty($nodeSecond);
 }
 
 function isItemDeleted($key, array $dataFirst, array $dataSecond)
 {
-    return array_key_exists($key, $dataFirst) && !array_key_exists($key, $dataSecond);
+    $nodeFirst  = getCurrentNode($key, $dataFirst);
+    $nodeSecond = getCurrentNode($key, $dataSecond);
+
+    return !empty($nodeFirst) && empty($nodeSecond);
 }
 
-function getReadableOutput($data)
+function getCurrentNode($key, $data)
 {
-    $logicData = [
-        true  => "true",
-        false => "false",
-        null  => "null",
-        ''    => "''"
+    $filterEqualItems = fn ($item) => $item['key'] === $key;
+    $currentList  = Collection\first((array_filter($data, $filterEqualItems)));
+
+    return $currentList;
+}
+
+function makeTree($data)
+{
+    $func = function ($key, $value) {
+        if (is_array($value)) {
+            return ['key' => $key, 'state' => 'default', 'value' => makeTree($value)];
+        }
+        return ['key' => $key, 'state' => 'default', 'value' => $value];
+    };
+
+    return array_map($func, array_keys($data), $data);
+}
+
+// TODO: module render()
+// -------------------RENDER PART-----------------------------
+
+function genOutput($tree)
+{
+    $iter = function ($node) {
+        return iter($node);
+    };
+
+    return implode("\n", array_map($iter, $tree));
+}
+
+function iter($node)
+{
+    print_r($node);
+    ['key' => $key, 'state' => $state, 'value' => $value] = $node;
+
+    if (is_array($value) && $state !== 'change') {
+        return genOutput($value);
+    }
+    
+    if ($state === 'change') {
+        $before = $value['before'];
+        $after  = $value['after'];
+
+        if (is_array($before)) {
+            return genOutput($before);
+        }
+
+        if (is_array($after)) {
+            return genOutput($after);
+        }
+    }
+
+    return getReadablePair($node);
+}
+
+function getReadablePair($data)
+{
+    $str = [
+        'default' => fn($keyName, $value) => "  {$keyName}: {$value}",
+        'same'    => fn($keyName, $value) => "  {$keyName}: {$value}",
+        'new'     => fn($keyName, $value) => "  + {$keyName}: {$value}",
+        'delete'  => fn($keyName, $value) => "  - {$keyName}: {$value}",
+        'change'  => fn($keyName, $value) => "  - {$keyName}: {$value['before']}\n  + {$keyName}: {$value['after']}\n"
     ];
 
-    $func = function ($value) use ($logicData) {
-        $flatten = array_map(function ($key, $value) use ($logicData) {
-            $value = $logicData[$value] ?? $value;
-            return "    {$key}: {$value}";
-        }, array_keys($value), $value);
+    ['key' => $key, 'state' => $state, 'value' => $value] = $data;
 
-        return $flatten;
+    return $str[$state]($key, $value);
+}
+
+function getPlainTreeOutput($tree)
+{
+    $iter = function ($node) {
+        return getReadablePair($node);
     };
-    $readableOutput = Collection\flatten(array_map($func, $data));
 
-    return "{\n" . implode("\n", $readableOutput) . "\n}";
+    return array_map($iter, $tree);
+}
+
+
+function getFileContent($file)
+{
+    $filePath = realpath($file);
+    if (!file_exists($filePath)) {
+        throw new \Exception('File does not found.');
+    }
+
+    $fileExtension = pathinfo($filePath, PATHINFO_EXTENSION);
+    $fileHandler   = fileHandlers($fileExtension);
+    $contentOfFile = file_get_contents($filePath);
+
+    return $fileHandler($contentOfFile);
+}
+
+function fileHandlers($fileExtension)
+{
+    $handlers = [
+        'json' => fn($data) => getJsonData($data),
+        'yml'  => fn($data) => getYamlData($data),
+        'yaml' => fn($data) => getYamlData($data)
+    ];
+
+    return $handlers[$fileExtension];
 }
